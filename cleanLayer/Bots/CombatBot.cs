500f3f040b4ecfb44aa273b19fe361a43d0d5194
Michael <micxplosive@gmail.com>
2011-11-06 23:35:58 +0100

    - Added a CombatBot for (pure) combat debugging

---
+++ cleanLayer/Bots/CombatBot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cleanCore;
+using cleanLayer.Library;
+using cleanLayer.Library.FSM;
+
+namespace cleanLayer.Bots
+{
+    public class CombatBot : BotBase
+    {
+        public override string Name
+        {
+            get { return "Pure Combat"; }
+        }
+
+        public override Form BotForm
+        {
+            get { return null; }
+        }
+
+        private bool _isRunning = false;
+        public override bool IsRunning
+        {
+            get { return _isRunning; }
+        }
+
+        public override bool Start()
+        {
+            if (!Manager.IsInGame)
+                return false;
+            _isRunning = true;
+            return true;
+        }
+
+        public override void Stop()
+        {
+            _isRunning = false;
+        }
+
+        public override void Pulse()
+        {
+            if (!_isRunning)
+                return;
+            if (!Manager.IsInGame)
+                return;
+            if (Combat.Brain == null)
+                return;
+            Combat.Pulse();
+        }
+    }
+}
